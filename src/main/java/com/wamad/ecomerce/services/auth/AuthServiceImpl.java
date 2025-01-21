1fddfe228c1152d07d8677c5fbe288dae5914285
walidmadad <walid.madad123@gmail.com>
2025-01-21 08:58:37 +0100

    add AuthServiceImpl

---
+++ src/main/java/com/wamad/ecomerce/services/auth/AuthServiceImpl.java
@@ -0,0 +1,39 @@
+package com.wamad.ecomerce.services.auth;
+
+import com.wamad.ecomerce.dto.SignupRequest;
+import com.wamad.ecomerce.dto.UserDto;
+import com.wamad.ecomerce.entity.User;
+import com.wamad.ecomerce.enums.UserRole;
+import com.wamad.ecomerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDto creatUser(SignupRequest signupRequest){
+        User user = new User();
+        user.setEmail(signupRequest.getEmail());
+        user.setName(signupRequest.getName());
+        user.setPassword(new BCryptPasswordEncoder().encode(signupRequest.getPassword()));
+        user.setRole(UserRole.CUSTOMER);
+        User createdUser = userRepository.save(user);
+
+        UserDto userDto = new UserDto();
+        userDto.setId(createdUser.getId());
+        return userDto;
+    }
+
+    @Override
+    public boolean hasUserWithEmail(String email){
+        return userRepository.findFirstByEmail(email).isPresent();
+    }
+}
