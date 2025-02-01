78a6bddfcbe98ae9f1f5b22dcab4fe23ee1818a1
walidmadad <walid.madad123@gmail.com>
2025-02-01 18:54:40 +0100

    add Valid annotation for check if the email address is valid or not

--- src/main/java/com/wamad/ecomerce/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/wamad/ecomerce/api/controller/auth/AuthenticationController.java
@@ -1,12 +1,13 @@
 package com.wamad.ecomerce.api.controller.auth;
 
 import com.wamad.ecomerce.api.model.RegistrationBody;
+import com.wamad.ecomerce.exception.UserAlreadyExistException;
 import com.wamad.ecomerce.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,7 +17,12 @@ public class AuthenticationController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public void registerUser(@RequestBody RegistrationBody registrationBody){
-        userService.registerUser(registrationBody);
+    public ResponseEntity registerUser(@Valid @RequestBody RegistrationBody registrationBody){
+        try {
+            userService.registerUser(registrationBody);
+            return ResponseEntity.ok().build();
+        } catch (UserAlreadyExistException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
--- src/main/java/com/wamad/ecomerce/api/model/RegistrationBody.java
+++ src/main/java/com/wamad/ecomerce/api/model/RegistrationBody.java
@@ -1,11 +1,15 @@
 package com.wamad.ecomerce.api.model;
 
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class RegistrationBody {
     private String username;
+
+    @Email
     private String email;
+
     private String password;
     private String firstName;
     private String lastName;
