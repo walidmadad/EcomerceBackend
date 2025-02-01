b1c517b10d4b34439c1762a493e399ab9c2a5d41
walidmadad <walid.madad123@gmail.com>
2025-02-01 14:36:53 +0100

    update User model

--- src/main/java/com/wamad/ecomerce/model/User.java
+++ src/main/java/com/wamad/ecomerce/model/User.java
@@ -4,6 +4,9 @@ import com.wamad.ecomerce.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 @Table(name = "users")
@@ -11,14 +14,24 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id", nullable = false)
     private Long id;
 
-    private String email;
-    private String password;
-    private String name;
-    private UserRole role;
+    @Column(name= "username", nullable = false, unique = true)
+    private String username;
 
-    @Lob
-    @Column(columnDefinition = "longblob")
-    private byte[] img;
+    @Column(name = "email", nullable = false, unique = true, length = 300)
+    private String email;
+
+    @Column(name="password", nullable = false, length = 1000)
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
 }
