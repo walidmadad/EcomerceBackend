c459b6d4ff61367d76a6e6b3efdbd0806209c983
walidmadad <walid.madad123@gmail.com>
2025-02-01 19:15:12 +0100

    enhance RegistrationBody validation rules

--- src/main/java/com/wamad/ecomerce/api/model/RegistrationBody.java
+++ src/main/java/com/wamad/ecomerce/api/model/RegistrationBody.java
@@ -1,16 +1,32 @@
 package com.wamad.ecomerce.api.model;
 
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class RegistrationBody {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String username;
 
+    @NotNull
+    @NotBlank
     @Email
     private String email;
 
+    @NotBlank
+    @NotNull
+    @Size(min = 8, max = 32)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")
     private String password;
+
+    @NotBlank
+    @NotNull
     private String firstName;
+
+    @NotBlank
+    @NotNull
     private String lastName;
 }
