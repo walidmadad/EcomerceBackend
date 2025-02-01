b49da47ddb29d1770f8ff076cedc0d428361dc13
walidmadad <walid.madad123@gmail.com>
2025-02-01 18:22:27 +0100

    update UserDao

--- src/main/java/com/wamad/ecomerce/model/dao/UserDAO.java
+++ src/main/java/com/wamad/ecomerce/model/dao/UserDAO.java
@@ -3,7 +3,9 @@ package com.wamad.ecomerce.model.dao;
 import com.wamad.ecomerce.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserDAO extends CrudRepository<User, Long> {
-    User findByEmail(String email);
-    User findByUsername(String username);
+    Optional<User>  findByEmail(String email);
+    Optional<User>  findByUsername(String username);
 }
