e98120311a96ab399b49d172ee84e8ca1417ae4f
walidmadad <walid.madad123@gmail.com>
2025-02-01 23:30:33 +0100

    add JWTRequestFilter

---
+++ src/main/java/com/wamad/ecomerce/api/security/JWTRequestFilter.java
@@ -0,0 +1,48 @@
+package com.wamad.ecomerce.api.security;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.wamad.ecomerce.model.User;
+import com.wamad.ecomerce.model.dao.UserDAO;
+import com.wamad.ecomerce.service.JWTService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JWTRequestFilter extends OncePerRequestFilter {
+
+    private final JWTService jwtService;
+    private final UserDAO userDAO;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenHeader = request.getHeader("Authorization");
+        if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+            String token = tokenHeader.substring(7);
+            try {
+                String username = jwtService.getUsername(token);
+                Optional<User> opUser = userDAO.findByUsername(username);
+                if (opUser.isPresent()) {
+                    User user = opUser.get();
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, new ArrayList());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } catch (JWTDecodeException ex) {
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
