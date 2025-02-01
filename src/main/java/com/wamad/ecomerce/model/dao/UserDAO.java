0fa8582cf4fdec3599e607c3d38055c9d4b926bc
walidmadad <walid.madad123@gmail.com>
2025-02-01 18:21:15 +0100

    add findByEmail and findByUsername on UserDAO

--- src/main/java/com/wamad/ecomerce/model/dao/UserDAO.java
+++ src/main/java/com/wamad/ecomerce/model/dao/UserDAO.java
@@ -4,5 +4,6 @@ import com.wamad.ecomerce.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserDAO extends CrudRepository<User, Long> {
-
+    User findByEmail(String email);
+    User findByUsername(String username);
 }
