db84f81200c6ddf9d6b10b919403ef9b8102299c
walidmadad <walid.madad123@gmail.com>
2025-02-01 23:31:52 +0100

    add getUsername using token

--- src/main/java/com/wamad/ecomerce/service/JWTService.java
+++ src/main/java/com/wamad/ecomerce/service/JWTService.java
@@ -35,4 +35,8 @@ public class JWTService {
                 .withIssuer(issuer)
                 .sign(algorithm);
     }
+
+    public String getUsername(String token) {
+        return JWT.decode(token).getClaim(USERNAME_KEY).asString();
+    }
 }
