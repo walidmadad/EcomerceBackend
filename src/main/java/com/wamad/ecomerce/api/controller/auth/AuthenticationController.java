15db025b79de4cda637c9289b299de7469995248
walidmadad <walid.madad123@gmail.com>
2025-02-02 17:27:52 +0100

    update loginUser Exception

--- src/main/java/com/wamad/ecomerce/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/wamad/ecomerce/api/controller/auth/AuthenticationController.java
@@ -3,7 +3,9 @@ package com.wamad.ecomerce.api.controller.auth;
 import com.wamad.ecomerce.api.model.LoginBody;
 import com.wamad.ecomerce.api.model.LoginResponse;
 import com.wamad.ecomerce.api.model.RegistrationBody;
+import com.wamad.ecomerce.exception.EmailFaiureException;
 import com.wamad.ecomerce.exception.UserAlreadyExistException;
+import com.wamad.ecomerce.exception.UserNotVerifiedException;
 import com.wamad.ecomerce.model.User;
 import com.wamad.ecomerce.service.UserService;
 import jakarta.validation.Valid;
@@ -27,20 +29,38 @@ public class AuthenticationController {
             return ResponseEntity.ok().build();
         } catch (UserAlreadyExistException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        } catch (EmailFaiureException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @PostMapping("/login")
     public ResponseEntity loginUser(@Valid @RequestBody LoginBody loginBody){
-        String jwt = userService.loginUser(loginBody);
+        String jwt = null;
+        try {
+            jwt = userService.loginUser(loginBody);
+        } catch (UserNotVerifiedException e) {
+            LoginResponse loginResponse = new LoginResponse();
+            loginResponse.setSuccess(false);
+            String reason = "USER_NOT_VERIFIED";
+            if (e.isNewEmailSent()){
+                reason += "_EMAIL_RESENT";
+            }
+            loginResponse.setMessage(reason);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(loginResponse);
+        } catch (EmailFaiureException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
         if (jwt == null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }else{
             LoginResponse loginResponse = new LoginResponse();
             loginResponse.setJwt(jwt);
+            loginResponse.setSuccess(true);
             return ResponseEntity.ok(loginResponse);
         }
     }
+
 
     @GetMapping("/me")
     public User getLoggedInUserProfile(@AuthenticationPrincipal User user) {
