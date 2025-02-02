03f3f3d417c3c638319080fdebf32df30856ba0f
walidmadad <walid.madad123@gmail.com>
2025-02-02 18:50:51 +0100

    add emailVerified attribute

--- src/main/java/com/wamad/ecomerce/model/User.java
+++ src/main/java/com/wamad/ecomerce/model/User.java
@@ -38,6 +38,10 @@ public class User {
     private List<Address> addresses = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("id desc")
     private List<VerificationToken> verficationTokens = new ArrayList<>();
+
+    @Column(name = "email_verified", nullable = false)
+    private boolean emailVerified = false;
 
 }
