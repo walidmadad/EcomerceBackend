8979e63bcf967ff98cd56d4cbe59a711087ff2d3
walidmadad <walid.madad123@gmail.com>
2025-02-02 17:33:29 +0100

    add creatVerificationToken method to UserService.java

--- src/main/java/com/wamad/ecomerce/service/UserService.java
+++ src/main/java/com/wamad/ecomerce/service/UserService.java
@@ -2,13 +2,19 @@ package com.wamad.ecomerce.service;
 
 import com.wamad.ecomerce.api.model.LoginBody;
 import com.wamad.ecomerce.api.model.RegistrationBody;
+import com.wamad.ecomerce.exception.EmailFaiureException;
 import com.wamad.ecomerce.exception.UserAlreadyExistException;
+import com.wamad.ecomerce.exception.UserNotVerifiedException;
 import com.wamad.ecomerce.model.User;
+import com.wamad.ecomerce.model.VerificationToken;
 import com.wamad.ecomerce.model.dao.UserDAO;
+import com.wamad.ecomerce.model.dao.VerificationTokenDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +23,10 @@ public class UserService {
     private final UserDAO userDAO;
     private final EncryptionService encryptionService;
     private final JWTService jwtService;
+    private final EmailService emailService;
+    private final VerificationTokenDAO verificationTokenDAO;
 
-    public User registerUser(RegistrationBody registrationBody) throws UserAlreadyExistException{
+    public User registerUser(RegistrationBody registrationBody) throws UserAlreadyExistException, EmailFaiureException {
         if(userDAO.findByEmail(registrationBody.getEmail()).isPresent() || userDAO.findByUsername(registrationBody.getUsername()).isPresent()){
             throw new UserAlreadyExistException();
         }
@@ -30,10 +38,21 @@ public class UserService {
 
         String encryptedPassword = encryptionService.encryptPassword(registrationBody.getPassword());
         user.setPassword(encryptedPassword);
+        VerificationToken verificationToken = createVerificationToken(user);
+        emailService.sendVerificationEmail(verificationToken);
         return userDAO.save(user);
     }
 
-    public String loginUser(LoginBody loginBody){
+    private VerificationToken createVerificationToken(User user){
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(jwtService.generateJWT(user));
+        verificationToken.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        verificationToken.setUser(user);
+        user.getVerficationTokens().add(verificationToken);
+        return verificationToken;
+    }
+
+    public String loginUser(LoginBody loginBody) throws UserNotVerifiedException, EmailFaiureException {
         Optional<User> userOptional = userDAO.findByEmail(loginBody.getEmail());
         if(userOptional.isEmpty()){
             userOptional = userDAO.findByUsername(loginBody.getUsername());
@@ -41,7 +60,19 @@ public class UserService {
         if(userOptional.isPresent()){
             User user = userOptional.get();
             if(encryptionService.checkPassword(loginBody.getPassword(), user.getPassword())){
-                return jwtService.generateJWT(user);
+                if(user.isEmailVerified()) {
+                    return jwtService.generateJWT(user);
+                } else{
+                    List<VerificationToken> verificationTokenList = user.getVerficationTokens();
+                    boolean resend = verificationTokenList.size() == 0 ||
+                            verificationTokenList.get(0).getCreatedAt().before(new Timestamp(System.currentTimeMillis() - (60 * 60 * 1000)));
+                    if(resend){
+                        VerificationToken verificationToken = createVerificationToken(user);
+                        verificationTokenDAO.save(verificationToken);
+                        emailService.sendVerificationEmail(verificationToken);
+                    }
+                    throw new UserNotVerifiedException(resend);
+                }
             }
         }
         return null;
