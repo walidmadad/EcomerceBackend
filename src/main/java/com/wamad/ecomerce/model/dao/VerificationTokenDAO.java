00f492dbf49ffb095a5dd8320e0f1c8f3490a457
walidmadad <walid.madad123@gmail.com>
2025-02-02 18:50:08 +0100

    fix bugs

--- src/main/java/com/wamad/ecomerce/model/dao/VerificationTokenDAO.java
+++ src/main/java/com/wamad/ecomerce/model/dao/VerificationTokenDAO.java
@@ -1,7 +1,12 @@
 package com.wamad.ecomerce.model.dao;
 
+import com.wamad.ecomerce.model.User;
 import com.wamad.ecomerce.model.VerificationToken;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Optional;
+
 public interface VerificationTokenDAO extends ListCrudRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+    void deleteByUser(User user);
 }
