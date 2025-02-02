3bb0e309d37beb1cac461c05fb8933cab43b0b25
walidmadad <walid.madad123@gmail.com>
2025-02-02 15:38:33 +0100

    add Verification Entity

---
+++ src/main/java/com/wamad/ecomerce/model/VerficationToken.java
@@ -0,0 +1,27 @@
+package com.wamad.ecomerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@Table(name = "verification_token")
+public class VerficationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Lob
+    @Column(name = "token", nullable = false, unique = true)
+    private String token;
+
+    @Column(name = "create_at", nullable = false)
+    private Timestamp createdAt;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
