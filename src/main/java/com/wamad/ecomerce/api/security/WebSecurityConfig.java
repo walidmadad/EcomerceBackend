cd5239a79c3ec6fe9bb58d8b30b5287cef69520e
walidmadad <walid.madad123@gmail.com>
2025-02-02 14:54:01 +0100

    add Endpoint Security

--- src/main/java/com/wamad/ecomerce/api/security/WebSecurityConfig.java
+++ src/main/java/com/wamad/ecomerce/api/security/WebSecurityConfig.java
@@ -1,16 +1,25 @@
 package com.wamad.ecomerce.api.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JWTRequestFilter jwtRequestFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.addFilterBefore(jwtRequestFilter, AuthenticationFilter.class);
+        http.authorizeHttpRequests(
+                auth ->  auth.requestMatchers("/product/**", "/auth/login", "/auth/register").permitAll()
+                        .anyRequest().authenticated());
         return http.build();
     }
 }
