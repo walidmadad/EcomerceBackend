6e258c130fba1f0106540f950803121caf0ba9bf
walidmadad <walid.madad123@gmail.com>
2025-02-16 17:56:57 +0100

    update VerificationTokenDAO

--- src/main/java/com/wamad/ecomerce/model/dao/VerificationTokenDAO.java
+++ src/main/java/com/wamad/ecomerce/model/dao/VerificationTokenDAO.java
@@ -5,8 +5,10 @@ import com.wamad.ecomerce.model.VerificationToken;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface VerificationTokenDAO extends ListCrudRepository<VerificationToken, Long> {
     Optional<VerificationToken> findByToken(String token);
     void deleteByUser(User user);
+    List<VerificationToken> findByUser_IdOrderByIdDesc(Long id);
 }
