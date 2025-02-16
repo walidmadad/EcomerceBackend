0d5bd7c28ee53e124749204d3e95b670c049821e
walidmadad <walid.madad123@gmail.com>
2025-02-16 14:55:36 +0100

    add UserServiceTest.java

---
+++ src/test/java/com/wamad/ecomerce/service/UserServiceTest.java
@@ -0,0 +1,67 @@
+package com.wamad.ecomerce.service;
+
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import com.wamad.ecomerce.api.model.LoginBody;
+import com.wamad.ecomerce.api.model.RegistrationBody;
+import com.wamad.ecomerce.exception.EmailFaiureException;
+import com.wamad.ecomerce.exception.UserAlreadyExistException;
+import com.wamad.ecomerce.exception.UserNotVerifiedException;
+import com.wamad.ecomerce.model.User;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @RegisterExtension
+    private static GreenMailExtension greenMailExtension = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("springboot", "secret"))
+            .withPerMethodLifecycle(true);
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    @Transactional
+    public void testRegisterUser() throws MessagingException {
+        RegistrationBody body = new RegistrationBody();
+        body.setUsername("UserA");
+        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
+        body.setFirstName("FirstName");
+        body.setLastName("LastName");
+        body.setPassword("MySecretPassword123");
+        Assertions.assertThrows(UserAlreadyExistException.class,
+                () -> userService.registerUser(body), "Username should already be in use.");
+        body.setUsername("UserServiceTest$testRegisterUser");
+        body.setEmail("UserA@junit.com");
+        Assertions.assertThrows(UserAlreadyExistException.class,
+                () -> userService.registerUser(body), "Email should already be in use.");
+        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
+        Assertions.assertDoesNotThrow(() -> userService.registerUser(body),
+                "User should register successfully.");
+        Assertions.assertEquals(body.getEmail(), greenMailExtension.getReceivedMessages()[0]
+                .getRecipients(Message.RecipientType.TO)[0].toString());
+    }
+
+    @Test
+    @Transactional
+    public void testLoginUser() throws UserNotVerifiedException, EmailFaiureException {
+        LoginBody loginBody = new LoginBody();
+        loginBody.setUsername("UserA11");
+        loginBody.setPassword("PasswordA123");
+        Assertions.assertNull(userService.loginUser(loginBody), "Username not exist.");
+        loginBody.setUsername("UserA");
+        loginBody.setPassword("PasswordA");
+        Assertions.assertNull(userService.loginUser(loginBody), "Username not exist.");
+
+    }
+
+}
